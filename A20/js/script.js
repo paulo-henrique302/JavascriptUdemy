336998894b4f489e7c8f25699d2995b390b1864c
Paulo <paulo.henrique302@outlook.comgit config --global user.name paulo-henrique302git commit -m 'initial commit>
2021-12-29 10:08:25 -0300

    exercise completed

--- A20/js/script.js
+++ A20/js/script.js
@@ -1,19 +1,39 @@
-function detect() {
+const pessoas = []
+pessoas.push({nome: 'pedro'})
+
+function genObject(nome, sobrenome, peso, altura) {
+    return {
+        nome, sobrenome, peso, altura
+    }
+}
+
+function showArray(object) {
+    const paragrafo = document.querySelector('.content')
+    paragrafo.innerHTML += `<p>${object.nome} ${object.sobrenome} ${object.peso} ${object.altura}</p>`
+}
+
+function addToArray() {
     const form = document.querySelector(".form")
-  
-    // form.onsubmit = (evento) => {
-    //     console.log(evento)
-    //     evento.preventDefault()
-    //     alert(1)
-    // } 
+    let cont = 0;
+
+    let nome = form.querySelector('.nome')
+    let sobrenome = form.querySelector('.sobrenome')
+    let peso = form.querySelector('.peso')
+    let altura = form.querySelector('.altura')
 
     function recebeEventoForm(evento) {
         evento.preventDefault()
-        console.log('não foi envia')
+        console.log("deu sub")
+
+        const obj = genObject(nome.value, sobrenome.value, peso.value, altura.value)
+        pessoas.push(obj)
+        cont++
+        console.log(pessoas[cont])
+        showArray(obj)
     }
 
     form.addEventListener('submit', recebeEventoForm)
-
 }
 
-detect()
+addToArray()
+

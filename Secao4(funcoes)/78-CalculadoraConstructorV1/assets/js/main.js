85fdc31905960e3010b961d73a2ae55535522a2e
paulo-henrique302 <paulo.henrique302@outlook.com>
2022-01-14 23:28:01 -0300

    Finished 78V1

--- Secao4(funcoes)/78-CalculadoraConstructorV1/assets/js/main.js
+++ Secao4(funcoes)/78-CalculadoraConstructorV1/assets/js/main.js
@@ -1,25 +1,63 @@
+// all made with arrow functions to avoid 'this' referencing the wrong target,
+// since arrows preserve this to point only to the parent object.
+
 function Calculadora() {
 
     this.display = document.querySelector('.display')
     
-    this.inicia = function() {
+    this.inicia = () => {
         this.cliqueBotoes()
     }
 
-    this.btnParaDisplay = function(num) {
+    this.btnParaDisplay = (num) => {
         this.display.value += num
+    }
+
+    this.apagaUm = () => {
+        this.display.value = this.display.value.slice(0, -1)
+    }
+
+    this.clearDisplay = () => {this.display.value = ''}
+
+    this.realizaConta = () => {
+        
+        let conta = this.display.value
+        
+        try {
+            conta = eval(conta)
+
+            if(!conta) {
+                alert('Operação inválida!')
+                return
+            }
+
+            this.display.value = conta
+
+        } catch {
+            alert("Operação inválida!")
+            return
+        }
     }
 
     this.cliqueBotoes = () => {
 
-        document.addEventListener('click', function(e) {
+        document.addEventListener('click', (e) => {
             const elemento = e.target
 
             if(elemento.classList.contains('btn-num')) {
                 this.btnParaDisplay(elemento.innerText)
             }
-        
-        }.bind(this))
+            if(elemento.classList.contains('btn-clear')) {
+                this.clearDisplay()
+            }
+            if(elemento.classList.contains('btn-del')) {
+                this.apagaUm()
+            }
+            if(elemento.classList.contains('btn-eq')) {
+                this.realizaConta()
+            }
+            
+        })
     } 
 }
 

dd26b754e05c3727c97b36578fdfb3f0cdc7df51
paulo-henrique302 <paulo.henrique302@outlook.com>
2022-02-13 15:56:20 -0300

    finished the CPF validator - running fine

--- Secao6-Objetos/97-Webpage/assets/js/main.js
+++ Secao6-Objetos/97-Webpage/assets/js/main.js
@@ -13,29 +13,84 @@ const getClick = inputQuery => {
         const element = evnt.target
 
         if(element.classList.contains('btn-validar')) { 
-            validador(inputQuery.value) 
+            // validador(inputQuery.value)
+            const cpf = new Validador(inputQuery.value)
+            cpf.cleanCPF()
+            cpf.validateCPF()
+            cpf.showResult(cpf.validationResult)
         }
     })
 }
 
-function validador(inputCPF) {
+function Validador(pureCPF) {
     
-    let validationResult = false
-
-    // remove all the non-numeric characters
-    let cleanCPF = inputCPF.replace(/\D+/g, '')
-    CPFArray = Array.from(cleanCPF)
-    
-    
-    showResult(validationResult)
+    this.validationResult
+    this.pureCPF = pureCPF
 
 }
 
-const showResult = result => {
+Validador.prototype.cleanCPF = function() {
+    let cleanCPF = this.pureCPF.replace(/\D+/g, '')
+    this.CPFArray = Array.from(cleanCPF)
+    this.CPFArraySliced = this.CPFArray.slice(0, -2)
+}
 
+Validador.prototype.showResult = function(result) {
     const resultArea = document.querySelector(".validation-area")
 
     result === true ? resultArea.innerHTML = `<p style="background-color: rgb(67, 253, 67)">CPF validado com sucesso</p>` : resultArea.innerHTML = `<p style="background-color: rgb(255, 110, 110)">CPF inválido</p>`
 }
+
+Validador.prototype.finalCompare = function(cpfArray, cpfArraySliced) {
+
+    console.log(cpfArray)
+    console.log(cpfArraySliced)
+
+    for(let i=0; i<11; i++) {
+        if(cpfArraySliced[i] !== cpfArray[i]) return false
+    }
+    return true
+}   
+
+Validador.prototype.validateCPF = function() {
+    
+    let firstCount = 10, lastCount = 11
+    
+    const total = this.CPFArray.reduce((ac, number) => {
+
+        // somente os 9 primeiros digitos!
+        if(firstCount === 1 || firstCount === 0 ) return ac
+        ac += firstCount * Number(number)
+        --firstCount
+        return ac
+
+    }, 0)
+
+    let firstDig = 11 - (total % 11)
+    this.CPFArraySliced.push(firstDig > 9 ? '0' : String(firstDig))
+
+    // validação do segundo dígito
+
+    const total2 = this.CPFArray.reduce((ac, number) => {
+
+        // somente os 10 primeiros digitos!
+        if(lastCount === 1 || lastCount === 0 ) return ac
+        ac += lastCount * Number(number)
+        --lastCount
+        return ac
+
+    }, 0)
+    
+    let lastDig = 11 - (total2 % 11)
+
+    this.CPFArraySliced.push(lastDig > 9 ? '0' : String(lastDig))
+
+
+    this.finalCompare(this.CPFArray, this.CPFArraySliced) ? this.validationResult = true : this.validationResult = false
+
+    alert(this.validationResult)
+
+}
+
 
 extractInput()

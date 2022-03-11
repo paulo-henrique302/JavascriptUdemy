ef333253fef3059d6dcc8d830b2a6644c65aa56b
paulo-henrique302 <paulo.henrique302@outlook.com>
2022-03-10 21:46:44 -0300

    finished 107 - form validation completed

--- Secao7-POO/107-ValidaForms/assets/js/main.js
+++ Secao7-POO/107-ValidaForms/assets/js/main.js
@@ -11,10 +11,35 @@ class ValidaForm {
 
     handleSubmit(e) {
         e.preventDefault()
-        const valid = this.isValid(e)
+        const valid = this.isValid()
+        const validPasswords = this.validPassword()
+
+        if(valid && validPasswords) {
+            alert('Formulário enviado!')
+            this.form.submit()
+        }
     }
 
-    isValid(e) {
+    validPassword() {
+        let valid = true
+
+        const senha = this.form.querySelector('.senha')
+        const repetirSenha = this.form.querySelector('.repetir-senha')
+
+        if(senha.value !== repetirSenha.value) {
+            valid = false
+            this.createError(senha, 'Senha e repetir senha precisam ser iguais')
+            this.createError(repetirSenha, 'Senha e repetir senha precisam ser iguais')
+        }
+        if(senha.value.length < 6 || senha.value.length > 12) {
+            valid = false
+            this.createError(senha, 'Senha precisa estar entre 6 e 12 caracteres')
+        }
+
+        return valid
+    }
+
+    isValid() {
         let valid = true
         
         for(let errorTxt of this.form.querySelectorAll('.error-text')) {
@@ -30,14 +55,45 @@ class ValidaForm {
                 this.createError(input, `O campo "${label.slice(0, -1)}" não pode estar vazio.`)
                 valid = false
             }
+            if(input.classList.contains('cpf')) {
+                if(!this.validateCPF(input)) valid = false
+            }
+            if(input.classList.contains('usuario')) {
+                if(!this.validateUser(input)) valid = false
+            }
+            valid = true
+        }
+        return valid
+    }
+
+    validateUser(field) {
+        const user = field.value
+        let valid = true
+
+        if(user.length < 3 || user.length > 12) {
+            this.createError(field, `Usuário deverá ter entre 3 e 12 caracteres`)
+            valid = false
+        }
+        if(!user.match(/^[a-zA-Z0-9]+$/g)) {
+            this.createError(field, 'Usuário só poderá conter letras e/ou números')
         }
     }
+
 
     createError(field, msg) {
         const div = document.createElement('div')
         div.innerHTML = msg
         div.classList.add('error-text')
         field.insertAdjacentElement('afterend', div)
+    }
+
+    validateCPF(field) {
+        const cpf = new ValidaCPF(field.value)
+        if(!cpf.valida()) {
+            this.createError(field, 'CPF inválido')
+            return false
+        }
+        return true
     }
     
 }

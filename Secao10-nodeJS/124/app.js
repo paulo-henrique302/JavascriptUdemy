9bfab15733ecf4d9b720118616bd2c6e0c89aa73
paulo-henrique302 <paulo.henrique302@outlook.com>
2022-06-15 09:33:06 -0300

    added the NodeJS folder and created 124

---
+++ Secao10-nodeJS/124/app.js
@@ -0,0 +1,6 @@
+const path = require('path')
+const axios = require('axios')
+const {Pessoa} = require('./mod')
+
+console.log(Pessoa)
+
---
+++ Secao10-nodeJS/124/mod.js
@@ -0,0 +1,15 @@
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }   
+}
+
+const dado1 = '@@@@'
+const dado2 = '22222'
+
+module.exports = {
+    dado1, 
+    dado2, 
+    Pessoa
+}
+
